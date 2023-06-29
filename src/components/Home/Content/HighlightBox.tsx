8022331bbc76c44519d24c99d50d3528a62d597e
Maikel Maas <mmaas@sphereon.com>
2023-06-29 12:18:29 +0200

    VDX-224 Integrate Energy SHR design into Portal copy of FMDM
       - Updated HighlightBox;

--- src/components/Home/Content/HighlightBox.tsx
+++ src/components/Home/Content/HighlightBox.tsx
@@ -1,9 +1,9 @@
-import React, { ReactElement } from 'react'
+import React, { CSSProperties, ReactElement } from 'react'
 import styles from './HighlightBox.module.css'
 import Eye from '@images/eye.svg'
 import Catalogue from '@images/catalogueIcon.svg'
 import Markdown from '@components/@shared/Markdown'
-import Button from '@components/@shared/atoms/Button'
+import Button, { ButtonProps } from '@components/@shared/atoms/Button'
 
 const icons = {
   eye: <Eye />,
@@ -15,22 +15,32 @@ export default function HighlightBox({
   title,
   body,
   buttonLabel,
-  link
+  link,
+  style,
+  className,
+  button,
+  text
 }: {
   icon: keyof typeof icons
   title: string
   body: string
   buttonLabel: string
   link: string
+  style?: CSSProperties
+  className?: string
+  button?: ButtonProps
+  text?: {
+    className?: string
+  }
 }): ReactElement {
   return (
-    <div className={styles.container}>
+    <div style={style} className={`${styles.container} ${className}`}>
       <span className={styles.heading}>
         <span className={styles.icon}>{icons[icon]}</span>
         <h3>{title}</h3>
       </span>
-      <Markdown text={body} />
-      <Button style="primary" to={link}>
+      <Markdown {...text} text={body} />
+      <Button {...button} to={link}>
         {buttonLabel}
       </Button>
     </div>
--- src/components/Home/Content/index.tsx
+++ src/components/Home/Content/index.tsx
@@ -5,6 +5,7 @@ import HighlightBox from './HighlightBox'
 import content from '../../../../content/pages/home/content.json'
 import Container from '@components/@shared/atoms/Container'
 import Markdown from '@components/@shared/Markdown'
+import Button from '@shared/atoms/Button'
 
 interface HomeContentData {
   teaser: {
@@ -14,6 +15,12 @@ interface HomeContentData {
   points: {
     text: string
   }[]
+  getInvolved: {
+    title: string
+    text: string
+    buttonLabel: string
+    link: string
+  }
   firstTimeVisiting: {
     title: string
     text: string
@@ -23,11 +30,21 @@ interface HomeContentData {
 }
 
 export default function HomeContent(): ReactElement {
-  const { teaser, points, firstTimeVisiting }: HomeContentData = content
+  const { teaser, points, firstTimeVisiting, getInvolved }: HomeContentData =
+    content
 
   return (
     <Container className={styles.wrapper}>
-      <h2>{teaser.title}</h2>
+      <div
+        style={{
+          display: 'flex',
+          gap: '10.5%',
+          paddingLeft: '5%'
+        }}
+      >
+        <h2>{teaser.title}</h2>
+        <p id={styles['who-is']}>Who is Energy SHR</p>
+      </div>
       <div className={styles.container}>
         <div className={styles.teaser}>
           <Markdown text={teaser.text} />
@@ -41,14 +58,41 @@ export default function HomeContent(): ReactElement {
               </span>
             ))}
           </div>
-          <HighlightBox
-            icon="eye"
-            title={firstTimeVisiting.title}
-            body={firstTimeVisiting.text}
-            buttonLabel={firstTimeVisiting.buttonLabel}
-            link={firstTimeVisiting.link}
-          />
         </div>
+      </div>
+      <Button className={styles.learnMore} style="text" arrow to="#">
+        Learn more
+      </Button>
+      <div
+        style={{
+          width: '56.48%',
+          display: 'flex',
+          justifyContent: 'space-between',
+          paddingLeft: '5%'
+        }}
+      >
+        <HighlightBox
+          icon="eye"
+          title={getInvolved.title}
+          body={getInvolved.text}
+          buttonLabel={getInvolved.buttonLabel}
+          link={getInvolved.link}
+          style={{ background: '#48A4ED' }}
+          className={styles.highlightBox}
+          button={{ style: 'text', arrow: true }}
+        />
+        <HighlightBox
+          icon="eye"
+          title={firstTimeVisiting.title}
+          body={firstTimeVisiting.text}
+          buttonLabel={firstTimeVisiting.buttonLabel}
+          link={firstTimeVisiting.link}
+          style={{
+            background: '#42DBCA'
+          }}
+          className={styles.highlightBox}
+          button={{ style: 'text', arrow: true }}
+        />
       </div>
     </Container>
   )
