acd428482801e2434d8922f1f77fa64c4c948522
Niels Klomp <nklomp@sphereon.com>
2023-09-18 05:27:37 +0200

    fix: sphereon provider detection

--- src/@context/Web3.tsx
+++ src/@context/Web3.tsx
@@ -169,7 +169,7 @@ function Web3Provider({ children }: { children: ReactNode }): ReactElement {
   function isSphereonProvider(provider: any) {
     const isSphereonAgent =
       typeof provider === 'object' &&
-      'httpAgent' in provider &&
+      ('httpAgent' in provider || 'httpsAgent' in provider) &&
       (provider.host ===
         process.env.NEXT_PUBLIC_WEB3_HEADLESS_PROVIDER_HOST_AUTHENTICATED ||
         provider.host ===
